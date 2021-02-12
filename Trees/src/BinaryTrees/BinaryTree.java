a1ca75a37bdf487ecc02fd6f337252b959295bf9
pavanKumarbhalkey@gmail.com <pavanKumarbhalkey@gmail.com>
2021-02-12 14:40:29 -0700

    added iterative inorder code

--- Trees/src/BinaryTrees/BinaryTree.java
+++ Trees/src/BinaryTrees/BinaryTree.java
@@ -59,7 +59,35 @@ public class BinaryTree {
 		return result;
 	}
 	
-	
+	public ArrayList<Integer> inOrderTraversalIterative(Node node) {
+		ArrayList<Integer> result = new ArrayList<>();
+		
+		if(node == null) return result;
+		Stack<Node> stack  = new Stack<>();
+		
+		stack.push(node);
+		/* we need this variable in this traversal to keep track of the current node as we use it move till the left most 
+		 * node and the when we get null for the current node we can pop the stack which tracks the node which called
+		 *  the null pointer, then we move to right of it, if we encounter null we pop the node one level above since we
+		 *  finished traversing the entire left side of a node and need to move to its right so before  going we pop add it 
+		 *  to list and go and apply the same rules.
+		 *  when the stack is empty it means we traversed the entire tree.
+		 *  */
+		Node currentNode = node; 
+ 
+		while((currentNode != null ) ||  !(stack.isEmpty())) {
+			if(currentNode != null ) {
+				stack.push(currentNode);
+				currentNode = currentNode.left;
+			} else {
+				currentNode = stack.pop();
+				result.add(currentNode.data);
+				currentNode = currentNode.right;
+			}
+		}
+		
+		return result;
+	} 
 	public static void main(String[] args) {
 		
 			BinaryTree tree = new BinaryTree();
@@ -80,6 +108,8 @@ public class BinaryTree {
 	        tree.inOrderTraversal(tree.root);
 	        System.out.println("\nIterative Preorder traversal of binary tree is "); 
 	        System.out.println(tree.preOrderTraversalIterative(tree.root));
+	        System.out.println("\nIterative Inorder traversal of binary tree is "); 
+	        System.out.println(tree.inOrderTraversalIterative(tree.root));
 	}
 
 }

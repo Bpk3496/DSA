266fc69a068c6c6e7fa2446cd5cf7aa2b403ce5e
pavanKumarbhalkey@gmail.com <pavanKumarbhalkey@gmail.com>
2021-02-13 15:49:34 -0700

    need to fix iterative postorder using 1 stack

--- Trees/src/BinaryTrees/BinaryTree.java
+++ Trees/src/BinaryTrees/BinaryTree.java
@@ -89,34 +89,66 @@ public class BinaryTree {
 		return result;
 	}
 	
-	public ArrayList<Integer> postOrderTraversalIterative(Node node) {
-			ArrayList<Integer> result = new ArrayList<>();    
-	        if(root == null) return result;
-	    
-	        Stack<Node> stack1 = new Stack<>();
-	        Stack<Node> stack2 = new Stack<>();
-	      
-	        stack1.push(root);
-	      
-	        while(!stack1.isEmpty()) {
-	            node = stack1.pop();
-	            stack2.push(node);
-	            
-	            if(node.left != null) 
-	                stack1.push(node.left);
-	            if(node.right != null)
-	                stack1.push(node.right);
-	            
-	        }
-	        
-	       while(!stack2.isEmpty()) {
-	            node = stack2.pop();
-	            result.add(node.data);
-	        }
-	       
-	        return result;   
-	    }
+	public ArrayList<Integer> postOrderTraversalIterative1Stack(Node node) { /* @tushar reference*/
+		ArrayList<Integer> result = new ArrayList<>();    
+        if(root == null) return result;
+    
+        Stack<Node> stack1 = new Stack<>();        
+        
+        Node currentNode = node;
+        while(!stack1.isEmpty()  || currentNode != null ) {
+        	if(currentNode != null) {
+        		stack1.push(currentNode);
+        		currentNode = currentNode.left;
+        	}
+        	else {
+        		Node tempNode  = stack1.peek().right;
+	        		if(tempNode == null) {
+	        			tempNode = stack1.pop();
+	        			result.add(tempNode.data);
+	        			
+	        			while(!stack1.isEmpty()  && tempNode == stack1.peek().right) {
+	        				tempNode = stack1.pop();
+	        				result.add(tempNode.data);
+	        			}
+	        			
+	        		}
+	        		else 
+	        			currentNode = tempNode;
+        	}	
+        }
+        System.out.println("returing result as "+ result);
+        return result;
+	}
 	
+	public ArrayList<Integer> postOrderTraversalIterative2Stack(Node node) { // @tech dose reference
+		ArrayList<Integer> result = new ArrayList<>();    
+        if(root == null) return result;
+    
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
+      
+        stack1.push(root);
+      
+        while(!stack1.isEmpty()) {
+            node = stack1.pop();
+            stack2.push(node);
+            
+            if(node.left != null) 
+                stack1.push(node.left);
+            if(node.right != null)
+                stack1.push(node.right);
+            
+        }
+        
+       while(!stack2.isEmpty()) {
+            node = stack2.pop();
+            result.add(node.data);
+        }
+       
+        return result;   
+    }
+
 	public static void main(String[] args) {
 		
 			BinaryTree tree = new BinaryTree();
@@ -135,12 +167,18 @@ public class BinaryTree {
 	        tree.postOrderTraversal(tree.root);
 	        System.out.println("\nPostorder traversal of binary tree is "); 
 	        tree.inOrderTraversal(tree.root);
-	        System.out.println("\nIterative Preorder traversal of binary tree is "); 
+	        
+	        System.out.println("\n Iterative Preorder traversal of binary tree is "); 
 	        System.out.println(tree.preOrderTraversalIterative(tree.root));
-	        System.out.println("\nIterative Inorder traversal of binary tree is "); 
+	        
+	        System.out.println("\n Iterative Inorder traversal of binary tree is "); 
 	        System.out.println(tree.inOrderTraversalIterative(tree.root));
-	        System.out.println("\nIterative postorder traversal of binary tree is ");	      
-	        System.out.println(tree.postOrderTraversalIterative(tree.root));
+	        
+	        System.out.println("\n Iterative postorder traversal of binary tree using 1 Stack is ");	      
+	        System.out.println(tree.postOrderTraversalIterative1Stack(tree.root));
+	        
+	        System.out.println("\nIterative postorder traversal of binary tree using 2 Stack is ");	      
+	        System.out.println(tree.postOrderTraversalIterative2Stack(tree.root));
 	}
 
 }

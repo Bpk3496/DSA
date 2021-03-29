af5be92d8a5ed6433d40938a510726e8651e510d
pavanKumarbhalkey@gmail.com <pavanKumarbhalkey@gmail.com>
2021-03-28 17:43:01 -0700

    detect cycle in an undirected graph

--- Trees/src/Graphs/CycleUndirectedGraph.java
+++ Trees/src/Graphs/CycleUndirectedGraph.java
@@ -25,41 +25,80 @@ public class CycleUndirectedGraph {
 			System.out.println();
 		}
 	}
-	public boolean isCyclic(int child, int parent, boolean[] visited) {
-		System.out.println("visited array curr length is "+ visited.length+" array  index is   "+ child +" with value "+visited[child] );
-		visited[child] = true;
-		System.out.println(visited[child]);
-			for(int adjNodes : graph.get(child)) {
-				if(!visited[adjNodes]) {
-					if(isCyclic(adjNodes,child,visited))
-						return true;	
-				}
-				else if(adjNodes != parent) {
-					return true;
-				}
-			}
+//	public boolean isCyclic(int child, int parent, boolean[] visited) {
+//		System.out.println("visited array curr length is "+ visited.length+" array  index is   "+ child +" with value "+visited[child] );
+//		visited[child] = true;
+//		System.out.println(visited[child]);
+//			for(int adjNodes : graph.get(child)) {
+//				if(!visited[adjNodes]) {
+//					if(isCyclic(adjNodes,child,visited))
+//						return true;	
+//				}
+//				else if(adjNodes != parent) {
+//					return true;
+//				}
+//			}
+//		
+//		return false;
+//	}
+	
+	/* graph coloring method */
+	public boolean isCyclicUtil(int node, int[] visited) {
 		
+		if(visited[node] == 2) 
+			return true;
+		
+		//visited[node] = 1;
+		boolean flag = false;
+		
+		for(int i=0; i < graph.get(node).size();i++) {
+			if(visited[graph.get(node).get(i)] ==1)
+				visited[graph.get(node).get(i)] =2;
+			else {
+				flag = isCyclicUtil(graph.get(node).get(i), visited);
+				if(flag == true) return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean isCyclic() 
+	{
+		int visited[] = new int[graph.size()];
+		boolean flag =false;
+		
+		for(int i=0; i< graph.size();i++){
+			visited[i] = 1;
+			for(int j : graph.get(i)){
+				flag = isCyclicUtil(j, visited);
+				if(flag)
+					return true;
+			}
+			System.out.println(visited[i]);
+			visited[i] = 0;
+			System.out.println(visited[i]);
+		}
 		return false;
 	}
 	
 	public static void main(String[] args) {
 		CycleUndirectedGraph g = new CycleUndirectedGraph(5);
 		
-		g.addEdges(0, 1);
-		g.addEdges(0, 3);
-		g.addEdges(0, 4);
-		g.addEdges(1, 2);
-		g.addEdges(3, 4);
-		g.addEdges(2, 4);
+//		g.addEdges(0, 1);
+//		g.addEdges(0, 3);
+//		g.addEdges(0, 4);
+//		g.addEdges(1, 2);
+//		g.addEdges(3, 4);
+//		g.addEdges(2, 4);
 
-//		g.addEdges(1, 2); g.addEdges(2, 3);	g.addEdges(3, 4);
+		g.addEdges(1, 2); g.addEdges(2, 3);	g.addEdges(3, 4);
 		
 		boolean[] visited = new boolean[graph.size()];
 		
 		for(int i=0; i< visited.length;i++)
 			visited[i] = false;
-		boolean flag = g.isCyclic(1,-1, visited);
-		
+//		boolean flag = g.isCyclic(1,-1, visited);
+		boolean flag = g.isCyclic();
 		if(flag)
 			System.out.println("Given Graph is cyclic");
 		else
